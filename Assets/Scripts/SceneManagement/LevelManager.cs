6fd29942940c9de09a96be9c43ba102e81efd201
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2021-06-12 12:09:44 +0200

    pausemenu

--- Assets/Scripts/SceneManagement/LevelManager.cs
+++ Assets/Scripts/SceneManagement/LevelManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
     public readonly static int MAIN_MENU_ID = 0;
     public readonly static int GAME_ID = 1;
 
@@ -13,5 +15,24 @@ public class LevelManager : MonoBehaviour
 
     public void QuitGame() => Application.Quit();
     
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused= false;
+    }
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused= true;
+    }
 
 }
