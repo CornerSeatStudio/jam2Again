b8808000f36692ee3a4228a4e05cf187c0b55019
Nathan Yuen <63110473+water0300@users.noreply.github.com>
2021-06-13 12:44:16 +0800

    COINO imports

--- Assets/Scripts/Baddie.cs
+++ Assets/Scripts/Baddie.cs
@@ -9,12 +9,10 @@ public class Baddie : MonoBehaviour {
     public int Health {get; set; } = 1;
     public AIPath aIPath;
     public Collider2D Col {get; private set; }
-    
+    public Animator animator;
     public double atkDistance;
-
     public double coolDown;
     public double NextAttack;
-
     public MonoBehaviour player;
 
     public event Action<Baddie> onDeathEvent;     
@@ -33,10 +31,12 @@ public class Baddie : MonoBehaviour {
             transform.localScale = new Vector2(-1f, 1f);
         } else if(aIPath.desiredVelocity.x <= -0.01) {
             transform.localScale = new Vector2(1f, 1f);
-        } if(Time.time > NextAttack && checkAttackPosition()) {
-            attack();
-            NextAttack = Time.time + coolDown;
-        }
+        } 
+        
+        // if(Time.time > NextAttack && checkAttackPosition()) {
+        //     attack();
+        //     NextAttack = Time.time + coolDown;
+        // }
     }
     public void takeDamage(){
         Health--;
@@ -55,19 +55,19 @@ public class Baddie : MonoBehaviour {
     }
     
     //presumably, we would have the player take damage through a call here
-    public void attack(){
-        // THis is supposed to be a placeholder for a physical attack
-        Debug.Log("Player attacked");
+    // public void attack(){
+    //     // THis is supposed to be a placeholder for a physical attack
+    //     Debug.Log("Player attacked");
         
-    }
+    // }
 
-    //check if player is in range
-    bool checkAttackPosition(){
-        if (Vector2.Distance(player.transform.position, this.transform.position) < atkDistance){
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // //check if player is in range
+    // bool checkAttackPosition(){
+    //     if (Vector2.Distance(player.transform.position, this.transform.position) < atkDistance){
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
 }
